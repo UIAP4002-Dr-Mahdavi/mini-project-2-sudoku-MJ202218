432d37587deb74cf0c875ebd901ec153b480fc70
Mohammad <mohammadjafari18132@gmail.com>
2022-05-20 17:11:03 +0430

    second virsion

--- mainproject/main_game.cpp
+++ mainproject/main_game.cpp
@@ -54,7 +54,7 @@ public:
 
                     for(int j=0; j<3; j++){
                         if (cells[i][j]== "-1" || (i == row && j == column))continue;
-                       if (cells[row][column] == cells [i][j])return true;
+                        if (cells[row][column] == cells [i][j])return true;
                     }
                 }
                 return false;
@@ -93,7 +93,7 @@ public:
 
                     for(int j=3; j<6; j++){
                         if (cells[i][j]== "-1" || (i == row && j == column))continue;
-                       if (cells[row][column] == cells [i][j])return true;
+                        if (cells[row][column] == cells [i][j])return true;
                     }
                 }
                 return false;
@@ -132,7 +132,7 @@ public:
 
                     for(int j=6; j<9; j++){
                         if (cells[i][j]== "-1" || (i == row && j == column))continue;
-                       if (cells[row][column] == cells [i][j])return true;
+                        if (cells[row][column] == cells [i][j])return true;
                     }
                 }
                 return false;
@@ -167,7 +167,7 @@ public:
 };
 Check check;
 
-class random_num{
+/**class random_num{
 public:
     int n1 = 0, n2 = 0 , row = 0,column = 0;
 
@@ -178,64 +178,69 @@ public:
     }
 
     void rand2(){
-        int howmany = 0 ;
-        bool random_check;
-        bool square_33 = true;
-        srand(time(0));
+        for (int abz = 0; abz<3 ; abz ++){
+            int howmany = 0 ;
+            bool random_check;
+            bool square_33 = true;
+            srand(time(0));
 
-     howmany = rand1();
+            howmany = rand1();
 
-     int *check_repeat_row = new int [howmany];
-     int *check_repeat_column = new int [howmany];
-     for (int j =0 ;j<howmany;j++){
-         check_repeat_row[j]=-1;
-         check_repeat_column[j]=-1;
-     }
-     for (int i=0;i<howmany;i++){
-         do {
-             square_33 = true;
-             while (square_33){
-             row = rand()%9;
-             column = rand1()%9;
-             if (row /3 == n1 && column / 3 == n2){
-                 square_33 = false;
-                 n2++;
-                 if (n2 == 3){
-                     n1 ++;
-                     n2 == 0;
-                 }
-             }
-             }
-             random_check = false;
-             for (int j =0 ; j<howmany ; j++){
+            int *check_repeat_row = new int [howmany];
+            int *check_repeat_column = new int [howmany];
+            for (int j =0 ;j<howmany;j++){
+                check_repeat_row[j]=-1;
+                check_repeat_column[j]=-1;
+            }
+            for (int i=0;i<howmany;i++){
+                do {
+                    square_33 = true;
+                    while (square_33){
+                        srand(time(0));
+                        row = rand()% (((n1*3)+3) +(n1*3));
+                        column = rand1()% ((n2*3)+3) +(n2*3);
+                        if (row /3 == n1 && column / 3 == n2){
+                            square_33 = false;
+                            if (n2 == 3){
+                                n1 ++;
+                                n2 = 0;
+                            }
+                        }
+                    }
+                    random_check = false;
+                    for (int j =0 ; j<howmany ; j++){
 
-                 if (row == check_repeat_row[j] && column == check_repeat_column[j] ){
+                        if (row == check_repeat_row[j] && column == check_repeat_column[j] ){
 
-                     random_check = true;
-                 }
-             }
+                            random_check = true;
+                            break;
+                        }
+                    }
 
-         }
-         while (random_check);
-         for (int z = 0 ; z<howmany; z++){
-             if(check_repeat_row[z] == -1){
-                 check_repeat_row [z]= row;
-                 break;
-             }
+                }
+                while (random_check);
+                for (int z = 0 ; z<howmany; z++){
+                    if(check_repeat_row[z] == -1){
+                        check_repeat_row [z]= row;
+                        break;
+                    }
 
-         }
-         for (int z = 0 ; z<howmany; z++){
-             if(check_repeat_column[z] == -1){
-                 check_repeat_column [z]= column;
-                 break;
-             }
+                }
+                for (int z = 0 ; z<howmany; z++){
+                    if(check_repeat_column[z] == -1){
+                        check_repeat_column [z]= column;
+                        break;
+                    }
 
-         }
-         rand3 ();
+                }
+                rand3 ();
 
-     }
-    delete []check_repeat_row;
-     delete []check_repeat_column;
+            }
+            delete []check_repeat_row;
+            delete []check_repeat_column;
+            n2++;
+
+        }
     }
 
     int rand3 (){
@@ -246,17 +251,17 @@ public:
         bool ch_square33 = true;
         while (ch_row == true || ch_column == true || ch_square33 == true){
 
-         number_of_cells= rand1()% 9 +1;
-         QString s = QString::number(number_of_cells);
-         cells[row][column] = s;
-          ch_row =check.check_row(row , column);
-          ch_column = check.check_column(row , column);
-          ch_square33 = check.check_square(row , column);
+            number_of_cells= rand1()% 9 +1;
+            QString s = QString::number(number_of_cells);
+            cells[row][column] = s;
+            ch_row =check.check_row(row , column);
+            ch_column = check.check_column(row , column);
+            ch_square33 = check.check_square(row , column);
         }
-       return -1 ;
+        return -1 ;
     }
 
-};
+};**/
 main_game::main_game(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::main_game)
@@ -270,19 +275,50 @@ main_game::main_game(QWidget *parent) :
         }
 
     }
-    random_num random_rand;
-    random_rand.rand2();
-    for (int ij=0;ij<9;ij++){
+    cells[0][0] ="5";
+    cells[1][0] ="6";
+    cells[3][0] ="8";
+    cells[4][0] ="4";
+    cells[5][0] ="7";
+    cells[0][1] ="3";
+    cells[2][1] ="9";
+    cells[6][1] ="6";
+    cells[2][2] ="8";
+    cells[1][3] ="1";
+    cells[4][3] ="8";
+    cells[7][3] ="4";
+    cells[0][4] ="7";
+    cells[1][4] ="9";
+    cells[3][4] ="6";
+    cells[5][4] ="2";
+    cells[7][4] ="1";
+    cells[8][4] ="8";
+    cells[1][5] ="5";
+    cells[4][5] ="3";
+    cells[7][5] ="9";
+    cells[6][6] ="2";
+    cells[2][7] ="6";
+    cells[6][7] ="8";
+    cells[8][7] ="7";
+    cells[3][8] ="3";
+    cells[4][8] ="1";
+    cells[5][8] ="6";
+    cells[7][8] ="5";
+    cells[8][8] ="9";
+    //random_num random_rand;
+    //random_rand.rand2();
+    /**for (int ij=0;ij<9;ij++){
         for (int ji =0;ji<9 ;ji++){
             if (cells[ij][ji] != "-1"){
-              ui->tableWidget->item(ij,ji)->setText(cells[ij][ji]);
+                ui->lineEdit->setText(cells[ij][ji]);
+
             }
         }
-    }
+    }**/
     ui->lineEdit->setText(" GUIDE");
-   timer = new QTimer(this);
-   connect(timer,SIGNAL(timeout()),this,SLOT(myfunction()));
-   timer->start(1000);
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(myfunction()));
+    timer->start(1000);
     userpass information;
     int number_of_players = 0;
     fopen_s(&info, "Information.dat", "rb");
@@ -348,10 +384,10 @@ void main_game::on_tableWidget_cellChanged(int row, int column)
 
 void main_game::myfunction()
 {
-       QTime time = QTime::currentTime();
-       QString time_text = time.toString("hh : mm : ss");
+    QTime time = QTime::currentTime();
+    QString time_text = time.toString("hh : mm : ss");
 
-       ui->Datetime->setText("                                                                                       "+time_text);
+    ui->Datetime->setText("                                                                                       "+time_text);
 
 }
 
